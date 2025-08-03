375a4ea099b28338507122fd45bba28111572172
michaelmusty <michaelmusty@gmail.com>
2025-08-03 16:37:52 -0400

    update

--- generate_dessin.py
+++ generate_dessin.py
@@ -1025,9 +1025,8 @@ def generate_interactive_html(pos, straight, curves, stubs, white_perm, black_pe
                 link.download = filename;
                 link.href = canvas.toDataURL('image/png');
                 link.click();
-                         }};
-             img.src = url;
-            }});
+            }};
+            img.src = url;
         }};
     </script>
 </body>
